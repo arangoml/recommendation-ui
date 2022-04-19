f6af085d21c49249d0b83e467096699772be857c
Chris Woodward <cw00dw0rd@gmail.com>
2022-04-19 12:33:21 -0400

    updates model logic

--- src/store/index.js
+++ src/store/index.js
@@ -16,69 +16,12 @@ const store = createStore({
       currentQuery: 0,
       queryInfo: [
         {
-          "name": "AQL Collaborative Filtering",
-          "shortDescription": `Recommendation using collaborative filtering implemented in AQL....`,
-          "description": `
-          Recommendation using collaborative filtering implemented in AQL.  
-          The algorithm computes recommendations dynamically given a User ID (user-a).  
-          
-          The query traverses from  user-a through rating edges to movies and then back to the 
-          other users uses and uses Cosine Simularity to determine the N most similar users (similar-group) to user-a.  
-          
-          Then the query traverses from the similar-group to through rate edges to movies to identify to identify the 
-          most highly rated movies of the similarity-group that have not been rated by user-a and returns this list as the recommendation.
-          `,
-          "aqlQuery":"<p>  WITH Movie, User, rates  LET similarUsers =  (FOR movie, edge IN 1 OUTBOUND @userId rates // eg. userid = Users/1 GRAPH 'movie-knowledge-graph'  LET userA_ratings = edge.rating //TO_NUMBER(edge.ratings)  FOR userB, edge2 IN 1..1 INBOUND movie rates  FILTER userB._id != @userId  LET userB_ratings = edge2.rating //TO_NUMBER(edge2.ratings)  COLLECT userids=userB._id INTO g KEEP userB_ratings, userA_ratings  LET userA_len = SQRT(SUM (FOR r IN g[*].userA_ratings RETURN r*r))  LET userB_len = SQRT(SUM (FOR r IN g[*].userB_ratings RETURN r*r))  LET dot_product = SUM (FOR n IN 0..(LENGTH(g[*].userA_ratings) - 1) RETURN g[n].userA_ratings * g[n].userB_ratings)  LET cos_sim = dot_product/ (userA_len * userB_len)  SORT cos_sim DESC LIMIT @similarUserLimit  RETURN {userBs: userids,  cosine_similarity: cos_sim}  )  LET userA_RatedMovies = (FOR movie, edge IN 1..1 OUTBOUND @userId rates RETURN movie._key)  FOR userB in similarUsers  FOR movie ,ratesEdge IN 1..1 OUTBOUND userB.userBs rates   FILTER movie._key NOT IN userA_RatedMovies  COLLECT userA_UnratedMovie = movie  AGGREGATE ratingSum = SUM(ratesEdge.rating)   SORT ratingSum DESC  LIMIT ${movieRecommendationLimit}  RETURN {movie: userA_UnratedMovie, score : ratingSum}</p>",
-          "queryName": "recommendMoviesCollaborativeFilteringAQL"
+          "name": "",
+          "shortDescription": ``,
+          "description": ``,
+          "aqlQuery":"",
+          "queryName": ""
         },
-        {
-          "name": "Content Based ML Recommendations",
-          "shortDescription": `This is an implementation of content-based recommendation.  The model uses TFIDF to compute similarity between....`,
-          "description": `
-          This is an implementation of content-based recommendation.  The model uses TFIDF to compute similarity between movies.
-
-          An AQL query concatenates the  movie title, tagline, and overview from each movie from the Movie Knowledge Graph for machine learning.  The similarity between each movie is computed using TFIDF and FAISS to compute the top movie similarities.
-
-          The movie similarities are communicated as inferences to the Movie Knowledge Graph as similar movie edges with a similarity score.  An AQL query then finds the most highly rated movies by the given user (user-a) and then uses the movie similarity inference to compute the set of movies most like the highest rated movies from user-a.
-          `,
-          "aqlQuery":`<p>/*<br>
-          This query uses the embedding inference computed using ML and transferred to ArangoDB in similarMovie_Embedding_Inference<br>
-          The query works as follows:<br>
-          Given a user, what movies are similar to the user's highest rated (topRatedLimit) movies and return the most similar movies the user has not rated.<br>
-          */</p>
-          
-          <p>LET userRatedMovies = (FOR ratingEdge IN rates FILTER ratingEdge._from == @userId SORT ratingEdge.rating DESC RETURN PARSE_IDENTIFIER(ratingEdge._to).key)<br>
-           FOR ratingEdge IN rates <br>
-           FILTER ratingEdge._from == @userId<br>
-           SORT ratingEdge.rating DESC <br>
-           LIMIT @topRatedMovieLimit <br>
-           FOR similarMovieEdge IN similarMovie_Embedding_Inference<br>
-           FILTER similarMovieEdge._from == ratingEdge._to<br>
-           LET similarMovie = similarMovieEdge._to<br>
-           FILTER similarMovie NOT IN userRatedMovies //Don't recommend movies already rated<br>
-           //compound score is user rating factor / distance - talk to data scientist on how to do this in amore scientific way<br>
-           LET compoundScore = (ratingEdge.rating/5.0)/similarMovieEdge.distance<br>
-           //Aggregate ratings for duplicate similar movies<br>
-           COLLECT recommendedMovie = similarMovie AGGREGATE aggregateScore = MAX(compoundScore)<br>
-           SORT aggregateScore DESC<br>
-           FILTER DOCUMENT(recommendedMovie)!=null//Temp Work-around while determine cause of nulls<br>
-           LIMIT @movieRecommendationLimit<br>
-           RETURN {movie : DOCUMENT(recommendedMovie) , score : aggregateScore}</p>`,
-          "queryName": "recommendMoviesContentBasedML"
-        },
-        {
-          "name": "ML Embeddings Recommendation",
-          "shortDescription": `This is an implementation using matrix factorization. The model uses Matrix Factorization to compute similarity...`,
-          "description": `
-          This is an implementation using matrix factorization. The model uses Matrix Factorization to compute similarity between movies and similarity between users.
-          
-          An AQL query retrieves user ratings on movies from the Movie Knowledge Graph for machine learning.The similarity between each movie is computed using Matrix Factorization and FAISS to compute the top movie similarities.  The movie similarities are communicated as inferences to the Movie Knowledge Graph as similar movie edges with a similarity score.
-
-          An AQL query then finds the most highly rated movies by the given user (user-a) and then uses the movie similarity inference to compute the set of movies most like the highest rated movies from user-a.
-          `,
-          "aqlQuery":"<p>  WITH Movie, User, rates  LET similarUsers =  (FOR movie, edge IN 1 OUTBOUND @userId rates // eg. userid = Users/1 GRAPH 'movie-knowledge-graph'  LET userA_ratings = edge.rating //TO_NUMBER(edge.ratings)  FOR userB, edge2 IN 1..1 INBOUND movie rates  FILTER userB._id != @userId  LET userB_ratings = edge2.rating //TO_NUMBER(edge2.ratings)  COLLECT userids=userB._id INTO g KEEP userB_ratings, userA_ratings  LET userA_len = SQRT(SUM (FOR r IN g[*].userA_ratings RETURN r*r))  LET userB_len = SQRT(SUM (FOR r IN g[*].userB_ratings RETURN r*r))  LET dot_product = SUM (FOR n IN 0..(LENGTH(g[*].userA_ratings) - 1) RETURN g[n].userA_ratings * g[n].userB_ratings)  LET cos_sim = dot_product/ (userA_len * userB_len)  SORT cos_sim DESC LIMIT @similarUserLimit  RETURN {userBs: userids,  cosine_similarity: cos_sim}  )  LET userA_RatedMovies = (FOR movie, edge IN 1..1 OUTBOUND @userId rates RETURN movie._key)  FOR userB in similarUsers  FOR movie ,ratesEdge IN 1..1 OUTBOUND userB.userBs rates   FILTER movie._key NOT IN userA_RatedMovies  COLLECT userA_UnratedMovie = movie  AGGREGATE ratingSum = SUM(ratesEdge.rating)   SORT ratingSum DESC  LIMIT ${movieRecommendationLimit}  RETURN {movie: userA_UnratedMovie, score : ratingSum}</p>",
-          "queryName": "recommendMoviesEmbeddingML"
-        }
       ],
       topGenres: {},
       sortedGenres: [],
@@ -103,6 +46,7 @@ const store = createStore({
     recommendMoviesAction({commit, state}, data) {
       commit('loading', true);
       commit('queryInfo', data.index);
+      console.log(data.query)
       axios({
         url: `${state.APIURL}`,
         auth: {
@@ -166,7 +110,7 @@ const store = createStore({
           commit('loading', false);
     },
     async setAPIURL({commit, dispatch,state}, APIDATA) {
-      await commit('setCredentials', APIDATA)
+      await commit('setCredentials', APIDATA);
       await commit('updateAPIURL', APIDATA.API_URL);
       axios({
         url: `${state.APIURL}`,
@@ -178,19 +122,31 @@ const store = createStore({
         data: {
           query: `
           query {
-            allMovies (limit: 1) {
-              id
+            allModels {
+              name
+              description
+              query
+              components
+              notebook
+              recommend
+              explain
+              label
+              resources {
+                url
+                description
+                title
+                image
+              }
             }
           }
-          `
-      }
-    }).then((result) => {
+          `}
+    }).then(async (result) => {
+      state.queryInfo = result.data.data.allModels
       state.APIRESPONSE = result.status;
-      state.APIRESPONSE == 200 ? commit('updateRecommendationDescriptions') : commit('loading', false)
-      state.APIRESPONSE == 200 ? dispatch('recommendMoviesAction', {query: 'recommendMoviesCollaborativeFilteringAQL', user: 0, index: 0}) : commit('loading', false)
-
     })
-    .then(() => {
+    .then(async () => {
+      
+      await dispatch('recommendMoviesAction', {query: state.queryInfo[0].recommend.toString(), user: 0, index: 0})
       commit('loading', false)
     })
     .catch(e => {
